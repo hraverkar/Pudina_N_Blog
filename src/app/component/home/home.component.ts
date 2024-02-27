8a4f8111a8a3cd35a85ad8190035896bd92643ca
Harshal Raverkar <harshal.raverkar@gmail.com>
2024-02-27 09:30:47 +0530

    Salutation Added into home page

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -13,13 +13,27 @@ import { AboutComponent } from '../about/about.component';
 export class HomeComponent implements OnInit {
   //#region public property
   public BasicInformation: IbasicInforamtion | undefined;
-  //#endregion
+    //#endregion
   //#region public methods
   public constructor(private basicInfoService: BasicInfoService) {}
   public ngOnInit(): void {
     this.basicInfoService.data$.subscribe((res: IbasicInforamtion) => {
       this.BasicInformation = res;
     });
+    this.getSalutation();
+  }
+
+  public getSalutation(): string {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 0 && currentHour < 12) {
+      return 'Good morning';
+    } else if (currentHour >= 12 && currentHour < 16) {
+      return 'Good afternoon';
+    } else if (currentHour >= 16 && currentHour < 20) {
+      return 'Good evening';
+    } else {
+      return 'Good night';
+    }
   }
   //#endregion
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { SecurityContext, importProvidersFrom } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
-import { HttpClient, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
@@ -17,10 +21,12 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(withFetch()),
     importProvidersFrom(
       ReactiveFormsModule,
-      MarkdownModule.forRoot({ loader: HttpClient, sanitize: SecurityContext.NONE }),
+      MarkdownModule.forRoot({
+        loader: HttpClient,
+        sanitize: SecurityContext.NONE,
+      }),
       HttpClientModule,
       CommonModule
     ),
-  ]
-})
-  .catch((err) => console.error(err));
+  ],
+}).catch((err) => console.error(err));
