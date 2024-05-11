a413424cc07d37f3493f386cd1b6e188a5b1e519
Harshal Raverkar <harshal.raverkar@gmail.com>
2024-05-11 23:58:04 +0530

    code updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //accessToken: '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',
+  accessToken: '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',
   baseURL: 'https://api.github.com/graphql',
   userName: 'hraverkar',
 };
